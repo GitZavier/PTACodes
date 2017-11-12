cc22a3346bab06354456a0cea7715d3485f2d90d
JesusZhao <zzgdata@gmail.com>
2017-11-12 11:24:31 +0800

    modify

--- GPLT/L1-015.cpp
+++ GPLT/L1-015.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	int n;
 	char c;
-	cin >> n >> c;;
+	cin >> n >> c;
 	for(int i=0;i<round(n/2.0);i++){
 		for(int j=0;j<n;j++){
 			cout << c;
