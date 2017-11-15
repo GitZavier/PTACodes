f99d38533fe9e566434ba45bd1bc705f2665696b
JesusZhao <zzgdata@gmail.com>
2017-11-15 20:32:58 +0800

    format code

--- GPLT/L1-004.java
+++ GPLT/L1-004.java
@@ -6,9 +6,9 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		int F;
 		int C;
-		F  = input.nextInt();
+		F = input.nextInt();
 		input.close();
-		C = 5*(F-32)/9;
+		C = 5 * (F - 32) / 9;
 		System.out.printf("Celsius = %d\n", C);
 	}
 
