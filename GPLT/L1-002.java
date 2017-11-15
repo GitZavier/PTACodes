ec762dd5c3f3c0eb70f696e140a2e703af113af1
JesusZhao <zzgdata@gmail.com>
2017-11-15 20:08:42 +0800

    format code

--- GPLT/L1-002.java
+++ GPLT/L1-002.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
@@ -9,21 +9,26 @@ public class Main{
     input.close();
     if (n <= 1000 && n > 0) {
       int line = 0;
-      while (((line+1)*(1+2*(line+1)-1)/2) *2 -1 <= n) 
+      while (((line + 1) * (1 + 2 * (line + 1) - 1) / 2) * 2 - 1 <= n) {
         ++line;
-      int left = n - (line*(1+2*line-1)-1);  // 求多余符号的个数
-      for(int i=line;i>0;i--) {  // 上一半
-        for(int s=0;s<line-i;s++)
+      }
+      int left = n - (line * (1 + 2 * line - 1) - 1);
+      for (int i = line; i > 0; i--) {
+        for (int s = 0; s < line - i; s++) {
           System.out.print(" ");
-        for(int j=0;j<2*i-1;j++)
+        }
+        for (int j = 0; j < 2 * i - 1; j++) {
           System.out.printf("%s", str);
+        }
         System.out.println();
       }
-      for(int i=2; i<=line; i++) {  // 下一半
-        for(int s=i; s<line; s++)
+      for (int i = 2; i <= line; i++) {
+        for (int s = i; s < line; s++) {
           System.out.print(" ");
-        for(int j=0; j<2*i-1; j++)
+        }
+        for (int j = 0; j < 2 * i - 1; j++) {
           System.out.printf("%s", str);
+        }
         System.out.println();
       }
       System.out.println(left);
